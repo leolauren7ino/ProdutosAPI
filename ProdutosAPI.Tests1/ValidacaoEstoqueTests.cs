f5e78f0da8cbc54d73a2d4dc72c3bb1a3be363de
Leonardo Curcio <Leonardo.Curcio@autoneum.com>
2026-09-22 10:34:33 -0300

    Commit Test

--- ProdutosAPI.Tests1/ValidacaoEstoqueTests.cs
+++ ProdutosAPI.Tests1/ValidacaoEstoqueTests.cs
@@ -19,7 +19,10 @@ namespace ProdutosAPI.Tests
 
             // Assert
             Assert.True(estoqueInsuficiente);
+            
         }
+
+
 
         [Fact]
         public void NaoDeveIndicarEstoqueInsuficienteQuandoQuantidadeMenorOuIgualAoEstoqu()
